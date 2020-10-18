0263488678dfc018b4e07ad90692b34f71944051
Leandro Alves <leandrorralves@gmail.com>
2020-10-18 01:35:43 -0300

    first commit

---
+++ src/main/java/com/example/springcloudmysql/model/Contact.java
@@ -0,0 +1,26 @@
+package com.example.springcloudmysql.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Contact {
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+
+   @Column(name = "name")
+   private String name;
+   private String email;
+   private String phone;
+}
